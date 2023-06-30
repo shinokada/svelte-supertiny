b8a2def68196625e363d787e6d64e7211b61e904
Shinichi Okada <okada.shin@gmail.com>
2023-06-30 07:29:11 +0200

    docs: update README

---
+++ icons-images.md
@@ -0,0 +1,5 @@
+# Icon images
+
+<p align="center">
+<img width="544" src="https://raw.githubusercontent.com/shinokada/svelte-supertinyicons/main/static/images/supertinyicons.webp" />
+</p>
---
+++ src/routes/class/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Amazon, Yahoo, Zoom, Youtube } from '$lib';
+</script>
+<Amazon size="100" />
+<Amazon class="m-4" size="100" />
+<Yahoo style="padding:10px;" size="100" />
+<Zoom size="100" />
+<Youtube size="100" />
+
+<svelte:head>
+  <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" />
+</svelte:head>
