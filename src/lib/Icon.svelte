35c133af507ea1cae9c1e2982c7d4e29512df21c
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-01 17:34:18 +0100

    chore: lib-helpers and format

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -9,7 +9,7 @@
 
 <!--
 @component
-[Go to docs](https://svelte-supertiny.vercel.app/)
+[Go to docs](https://svelte-supertiny.codewithshin.com/)
 ## Props
 @prop export let icon: ComponentType;
 @prop export let size: number = 24;
